780dcdfdf346904a4379c0220281a66a9bbc39bc
lovelysusy924 <susana.fernandez.simontech.16@gmail.com>
2016-03-04 18:12:15 +0000

    project time, yo

---
+++ project-time/app.rb
@@ -0,0 +1,16 @@
+require_relative 'config/environment'
+
+class App < Sinatra::Base
+
+get '/' do
+  erb :dashboard
+end
+
+post '/' do
+  @user = {
+    name: "#{params["fname"]} #{params["lname"]}"
+  }
+  erb :index
+  end
+
+end
